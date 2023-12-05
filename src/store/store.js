3d0837c025091f8470a8c1e4fa348d61a50817ea
cgarcia <cgarcia@promax.es>
2023-12-05 14:53:23 +0100

    implement redux structure

---
+++ src/store/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "../Reducers/rootReducer.js";
+
+const state = {
+  notes : [],
+}
+
+const store = configureStore({ 
+  reducer: rootReducer, 
+  devTools: process.env.NODE_ENV !== 'production',
+  preloadedState: state,
+  enhancers: [],
+});
+
+export default store;
